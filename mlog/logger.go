94afb88b028b00f0f67f0484a18b49db9e53c524
有種 <39034242+kenzok8@users.noreply.github.com>
2026-03-19 12:33:15 +0800

    mlog: merge format-logtime patch into source (custom time encoder)

--- mlog/logger.go
+++ mlog/logger.go
@@ -1,15 +1,26 @@
 package mlog
 
-import "go.uber.org/zap"
+import (
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 var logger = mustInitLogger()
 
 func mustInitLogger() *zap.Logger {
-	l, err := zap.NewDevelopment(zap.WithCaller(false))
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeTime = customTimeEncoder
+	l, err := config.Build(zap.WithCaller(false))
 	if err != nil {
 		panic("failed to init mlog:" + err.Error())
 	}
 	return l
+}
+
+func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
 }
 
 func L() *zap.Logger {
